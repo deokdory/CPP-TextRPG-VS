a8005cd7cbb5dc384ed6a244ce585dfb600c6784
deokdory <deokdory@naver.com>
2023-03-02 16:54:25 +0900

    Update CompletedQuest.cpp

--- C-TextRPG-VS/CPP/Source/CompletedQuest.cpp
+++ C-TextRPG-VS/CPP/Source/CompletedQuest.cpp
@@ -6,83 +6,85 @@ CompletedQuest* CompletedQuest::Tail = nullptr;
 int CompletedQuest::Length = 0;
 
 CompletedQuest::CompletedQuest(int _index)
-    : completed_quest_index(_index), Next(nullptr) {}
+	: completed_quest_index(_index), Next(nullptr) {}
 
 void CompletedQuest::NewCompletedQuest(int _index) {
-  CompletedQuest* node = new CompletedQuest(_index);
-  node->Push();
+	CompletedQuest* node = new CompletedQuest(_index);
+	node->Push();
 }
 
 void CompletedQuest::Push() {
-  if (Head == nullptr) {
-    Head = this;
-    Tail = this;
-  }
+	if (Head == nullptr) {
+		Head = this;
+		Tail = this;
+	}
 
-  else {
-    Tail->Next = this;
-    Tail = this;
-  }
+	else {
+		Tail->Next = this;
+		Tail = this;
+	}
 
-  Length++;
+	Length++;
 }
 
 bool CompletedQuest::CheckIsCompletedQuest(int _index) {
-  if (Head == nullptr) {
-    return false;
-  }
+	if (Head == nullptr) {
+		return false;
+	}
 
-  CompletedQuest* checker = Head;
+	CompletedQuest* checker = Head;
 
-  while (true) {
-    if (checker->GetCompletedQuestIndex() == _index)
-      return true;
-    else if (checker->Next != nullptr)
-      checker = checker->Next;
-    else
-      return false;
-  }
+	while (true) {
+		if (checker->GetCompletedQuestIndex() == _index)
+			return true;
+		else if (checker->Next != nullptr)
+			checker = checker->Next;
+		else
+			return false;
+	}
 }
 
 void CompletedQuest::Remove() {
-  if (Head == this) {
-    if (this->Next != nullptr) {
-      Head = this->Next;
-      delete this;
-    } else {
-      Head = Tail = nullptr;
-      delete this;
-    }
-  }
+	if (Head == this) {
+		if (this->Next != nullptr) {
+			Head = this->Next;
+		}
+		else {
+			Head = Tail = nullptr;
+		}
+	}
+	else {
+		CompletedQuest* cur = Head;
 
-  CompletedQuest* cur = Head;
-
-  while (true) {
-    if (cur->Next == this) {
-      if (this->Next != nullptr) {
-        cur->Next = this->Next;
-      } else {
-        cur->Next = nullptr;
-      }
-      delete this;
-    } else {
-      cur = cur->Next;
-    }
-  }
-  Length--;
+		while (true) {
+			if (cur->Next != nullptr) {
+				if (cur->Next == this) {
+					cur->Next = this->Next;
+					break;
+				}
+				else { cur = cur->Next; }
+			}
+			else {
+				break;
+			}
+		}
+	}
+	delete this;
+	Length--;
 }
 
+
 void CompletedQuest::RemoveAll(CompletedQuest* node) {
-  Length = 0;
+	Length = 0;
 
-  if (Head == nullptr) {
-    std::cout << "CompletedQuestList is null" << std::endl;
-    system("pause");
-    return;
-  }
+	if (Head == nullptr) {
+		std::cout << "CompletedQuestList is null" << std::endl;
+		system("pause");
+		return;
+	}
 
-  if (Head->Next != nullptr) {
-    RemoveAll(Head->Next);
-  }
-  delete this;
+	if (Head->Next != nullptr) {
+		RemoveAll(Head->Next);
+	}
+	delete this;
 }
